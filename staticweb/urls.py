c3f355c9a78eeea33d661601e4c8fb3a4de18d5c
fahmirevo <fahmi.velosa1997@gmail.com>
2017-12-11 12:07:22 +0700

    add all html files

--- staticweb/urls.py
+++ staticweb/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-import .views as v
+from staticweb import views as v
 
 urlpatterns = [
-        url(r'^$', login_required(v.IndexView.as_view()), name="index"),
-        url(r'^signup/$', login_required(v.SignupView.as_view()), name="signup"),
-        url(r'^login/$', login_required(v.LoginView.as_view()), name="login"),
-        url(r'^explore/$', login_required(v.ExploreView.as_view()), name="explore"),
+        url(r'^$', v.IndexView.as_view(), name="index"),
+        url(r'^signup/$', v.SignupView.as_view(), name="signup"),
+        url(r'^login/$', v.LoginView.as_view(), name="login"),
+        url(r'^explore/$', v.ExploreView.as_view(), name="explore"),
         url(r'^dashboard/$', login_required(v.DashboardView.as_view()), name="dashboard"),
         url(r'^pending/$', login_required(v.PendingView.as_view()), name="pending"),
         url(r'^payment/$', login_required(v.PaymentView.as_view()), name="payment"),
--- staticweb/views.py
+++ staticweb/views.py
@@ -4,25 +4,25 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = "staticweb/index.html"
 
 class SignupView(TemplateView):
-    template_name = "signup.html"
+    template_name = "staticweb/signup.html"
 
 class LoginView(TemplateView):
-    template_name = "login.html"
+    template_name = "staticweb/login.html"
 
 class ExploreView(TemplateView):
-    template_name = "explore.html"
+    template_name = "staticweb/explore.html"
 
 class DashboardView(TemplateView):
-    template_name = "dashboard.html"
+    template_name = "staticweb/dashboard.html"
 
 class PendingView(TemplateView):
-    template_name = "pending.html"
+    template_name = "staticweb/pending.html"
 
 class PaymentView(TemplateView):
-    template_name = "payment.html"
+    template_name = "staticweb/payment.html"
 
 class TutorView(TemplateView):
-    template_name = "tutor.html"
+    template_name = "staticweb/tutor.html"

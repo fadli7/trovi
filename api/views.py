d6083b72c6eef0ca7d4b2a35dd9e1bab14bfb7f8
fahmirevo <fahmi.velosa1997@gmail.com>
2017-12-12 08:50:51 +0700

    create login function

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
+from django.http import JsonResponse
+from django.views import View
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 # Create your views here.
+
+class LoginView(View):
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({status: 'success'})
+
+        return JsonResponse({status: 'failed'})

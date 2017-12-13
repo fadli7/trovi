5bf7ada4b5f03b3ee26ef6a50aef277e36f9bd90
fahmirevo <fahmi.velosa1997@gmail.com>
2017-12-14 02:08:03 +0700

    fixed media root problem

--- api/models.py
+++ api/models.py
@@ -1,7 +1,34 @@
+import uuid
+import os
+from django.conf import settings
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.deconstruct import deconstructible
+
+@deconstructible
+class UploadToUUIDPath:
+
+    def __init__(self, path):
+        self.sub_path = path
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+
+        filename = '{}.{}'.format(uuid.uuid4(), ext)
+        return os.path.join(self.sub_path, filename)
+
 
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField()
+
+class Tutorial(models.Model):
+    name = models.CharField()
+    video = models.FileField(upload_to=UploadToUUIDPath(os.path.join(settings.MEDIA_ROOT), 'tutorial', 'video'))
+
+class Illustrations(models.Model):
+    image = models.ImageField(upload_to=UploadToUUIDPath(os.path.join(settings.MEDIA_ROOT, 'tutorial', 'img')))
+    tutorial = models.ManyToManyField(Tutorial)

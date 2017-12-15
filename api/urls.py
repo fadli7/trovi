a955a8944794231772e3622a4f3e94f4bddfe72b
fahmirevo <fahmi.velosa1997@gmail.com>
2017-12-15 19:40:55 +0700

    add registration url

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from api import views as v
+from api.views import Authview, RegistrationView, Authview
 
 urlpatterns = [
-        url(r'auth', v.AuthView.as_view()),
-        url(r'user', v.UserView.as_view()),
+        url(r'auth', AuthView.as_view()),
+        url(r'user', UserView.as_view()),
+        url(r'registration', RegistrationView.as_view()
         ]

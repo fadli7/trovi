b4d222c4114dc90ed2f61ca5b464131439af2170
fahmirevo <fahmi.velosa1997@gmail.com>
2017-12-23 16:13:30 +0700

    haul the database

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,12 @@ from django.contrib.auth.decorators import login_required
 
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from api.views import AuthView, RegistrationView, UserView, PageView
+from api.views import AuthView, RegistrationView, UserView, PageView, TutorialView
 
 urlpatterns = [
         url(r'auth', AuthView.as_view()),
         url(r'registration', RegistrationView.as_view()),
         url(r'user', login_required(UserView.as_view())),
         url(r'page', PageView.as_view()),
+        url(r'tutorial', login_required(TutorialView.as_view())),
         ]

ad02cb2cc773470595251ff3a164a9ff9255eb6d
fahmirevo <fahmi.velosa1997@gmail.com>
2017-12-26 00:27:37 +0700

    refine form and view

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from api.views import AuthView, RegistrationView, UserView, ExploreView, TutorialView, TutorialOwnedView, PendingView
+from api.views import AuthView, RegistrationView, UserView, ExploreView, TutorialView, TutorialOwnedView, PendingView, TransactionView
 
 urlpatterns = [
         url(r'^auth/$', AuthView.as_view()),
@@ -10,4 +10,7 @@ urlpatterns = [
         url(r'^user/$', login_required(UserView.as_view())),
         url(r'^explore/$', ExploreView.as_view()),
         url(r'^tutorial/$', login_required(TutorialView.as_view())),
+        url(r'^owned/$', login_required(TutorialOwnedView.as_view())),
+        url(r'^pending/$', login_required(PnedingView.as_view())),
+        url(r'^transaction/$', login_required(TransactionView.as_view())),
         ]
